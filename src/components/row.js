3e8b1189f2d667be2931ba55e8de55ad43385427
Kisiwatpong <Kisiwatpong@computerlogy.com>
2021-08-02 16:56:33 +0700

    test

--- src/components/row.js
+++ src/components/row.js
@@ -4,35 +4,39 @@ import Animated from 'react-native-reanimated';
 
 import style from '../style';
 
-const Row = memo(({ row, move, renderItem, hidden, onPress, onDragStartCallback }) => {
-  const onDragBegin = () => {
-    if (onDragStartCallback) {
-      onDragStartCallback();
-    }
-    const hoverComponent = renderItem({
+const Row = memo(
+  ({ row, move, renderItem, hidden, onPress, onDragStartCallback }) => {
+    const onDragBegin = () => {
+      if (onDragStartCallback) {
+        onDragStartCallback();
+      }
+      const hoverComponent = renderItem({
+        move,
+        item: row.data,
+        index: row.index,
+      });
+      move(hoverComponent, row);
+    };
+
+    const component = renderItem({
       move,
       item: row.data,
       index: row.index,
     });
-    move(hoverComponent, row);
-  };
 
-  const component = renderItem({
-    move,
-    item: row.data,
-    index: row.index,
-  });
-
-  return (
-    <TouchableWithoutFeedback
-      onLongPress={onDragBegin}
-      delayLongPress={300}
-      onPress={onPress}>
-      <Animated.View style={hidden ? style.invisible : style.visible}>
-        {component}
-      </Animated.View>
-    </TouchableWithoutFeedback>
-  );
-});
+    return (
+      <TouchableWithoutFeedback
+        onLongPress={onDragBegin}
+        delayLongPress={300}
+        onPress={onPress}
+      >
+        <Animated.View style={hidden ? style.invisible : style.visible}>
+          {component}
+          <Text>Test</Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    );
+  },
+);
 
 export default Row;
